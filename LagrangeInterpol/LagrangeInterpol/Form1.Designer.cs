79eba958af15f602f959158b5059b7222755c6b9
Nulianitskiy <nikitaul78@gmail.com>
2020-12-24 23:10:09 +0300

    v3
    LagrangeInterpol

---
+++ LagrangeInterpol/LagrangeInterpol/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace LagrangeInterpol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.Location = new System.Drawing.Point(0, 0);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            this.zedGraph.Size = new System.Drawing.Size(631, 453);
+            this.zedGraph.TabIndex = 0;
+            this.zedGraph.UseExtendedPrintDialog = true;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(637, 12);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(177, 32);
+            this.Calculate.TabIndex = 1;
+            this.Calculate.Text = "Вычислить";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(637, 51);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(177, 20);
+            this.textBox.TabIndex = 2;
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Enabled = false;
+            this.richTextBox.Location = new System.Drawing.Point(638, 78);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(176, 360);
+            this.richTextBox.TabIndex = 3;
+            this.richTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 450);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.zedGraph);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ LagrangeInterpol/LagrangeInterpol/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ZedGraph;
+
+namespace LagrangeInterpol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int MatrixLines(string file)// кол-во строк
+        {
+            List<string> lines = File.ReadAllLines(file).ToList();
+            return lines.Capacity;
+        }
+
+        void MatrixReader(double[,] Matrix, string file)
+        {
+            List<string> lines = File.ReadAllLines(file).ToList();
+            int n = Matrix.GetLength(1);
+            int i = 0;
+            foreach (string line in lines)
+            {
+                string[] entries = line.Split(' ');
+
+                for (int j = 0; j < n; j++)
+                    Matrix[i, j] = Double.Parse(entries[j]);
+                i++;
+            }
+        }//Чтение матрицы из файла
+        void PrintMatrix(double[,] Matrix)
+        {
+            int n = Matrix.GetLength(0);
+            int k = Matrix.GetLength(1);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                    richTextBox.Text += Matrix[i, j] + "   ";
+                richTextBox.Text += "\n";
+            }
+            richTextBox.Text += "///////////////////////////\n";
+        }//Вывод матрицы в текстбокс
+        public double P(double[,] Matrix, double root)//Pn(xi) = E  yk  * Lk(xi)
+        {                                            //     k=0 -> n
+            int n = Matrix.GetLength(0);
+            double c, s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                c = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                        c *= (root - Matrix[j,0]) / (Matrix[i,0] - Matrix[j,0]);
+                }
+                s += c * Matrix[i, 1];
+            }
+            return s;
+        }
+        public double W(double[,] Matrix, double root)//W   (x) = (x - x0)(x-x1)(x-x2)........(x-xn)    ,x == root
+        {                                            //  n+1                                             
+            int n = Matrix.GetLength(0);
+
+            double Rez = root - Matrix[0, 0];
+            for (int i = 1; i < n; i++)
+            {
+                Rez *= root - Matrix[i, 0];
+            }
+            return Rez;
+        }
+        public void graf(double[,] Matrix)
+        {
+            int n = Matrix.GetLength(0);
+
+            GraphPane pane = zedGraph.GraphPane;
+            pane.Title.Text = "";
+            pane.CurveList.Clear();
+            PointPairList list = new PointPairList();
+
+            double xmin = -100;
+            double xmax = 100;
+
+            for (double x = xmin; x <= xmax; x += 0.01)
+            {
+                list.Add(x, P(Matrix, x));
+            }
+            LineItem myCurve = pane.AddCurve("LagrangeInterpol", list, Color.Blue, SymbolType.None);
+
+            zedGraph.AxisChange();
+            zedGraph.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file = "In3.txt";
+
+            int n = MatrixLines(file);
+            double[,] Matrix = new double[n, 2];
+
+            MatrixReader(Matrix, file);
+            richTextBox.Text = "Xi   Yi\n";
+            PrintMatrix(Matrix);
+
+            double root = Convert.ToDouble(textBox.Text);
+            richTextBox.Text += "Корень = " + root;
+            richTextBox.Text += "\n/////////////////////\n";
+            richTextBox.Text += "P" + (n - 1) + "(" + root + ") = " + P(Matrix, root) + "\n";
+            richTextBox.Text += "W" + (n) + "(" + root + ") = " + W(Matrix, root) + "\n";
+            graf(Matrix);
+            
+        }
+
+    }
+}
